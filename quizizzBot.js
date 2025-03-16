3bf95a92002ba522f6786d3afbbc2b60c7556e24
ichiiiochaaa <indahnurcahyani1415@gmail.com>
2025-03-16 21:35:57 +0700

    Update quizizzBot.js

--- quizizzBot.js
+++ quizizzBot.js
@@ -1,4 +1,4 @@
-const pin = '00158336'; // Replace with your desired pin
+const pin = '60244664'; // Replace with your desired pin
 
 (async function () {
     // Function to fetch and categorize Quizizz answers using an API
